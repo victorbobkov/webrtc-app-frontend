a95347c8045bce84e476bdd0059dac73a202c4fd
victor bobkov <victorbobkov@gmail.com>
2022-02-21 00:51:43 +0300

    Handle pre-offer answer

--- src/utils/webRTCHandler.js
+++ src/utils/webRTCHandler.js
@@ -65,11 +65,25 @@ export const acceptIncomingCallRequest = () => {
 }
 
 export const rejectIncomingCallRequest = () => {
-   resetCallData()
    wss.sendPreOfferAnswer({
       callerSocketId: connectedUserSocketId,
       answer: preOfferAnswers.CALL_REJECTED
    })
+
+   resetCallData()
+}
+
+export const handlePreOfferAnswer = (data) => {
+   if (data.answer === preOfferAnswers.CALL_ACCEPTED) {
+      // send WebRTC offer
+   } else {
+      let rejectionReason
+      if (data.answer === preOfferAnswers.CALL_NOT_AVAILABLE) {
+         rejectionReason = 'Callee is not able to pick up the call right now'
+      } else {
+         rejectionReason = 'Call rejected by the callee'
+      }
+   }
 }
 
 export const checkIfCallIsPossible = () => {
